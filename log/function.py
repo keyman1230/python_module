05fdd2850f4edd81b0771c93c3652f414de775e3
DESKTOP-4FPBBQ2\User <twinbig@lginnotek.com>
2023-05-30 17:44:18 +0900

    File 정리

--- log/function.py
+++
@@ -1,6 +0,0 @@
-import log
-
-def test2():
-    logger = log.set_log(module=__name__)
-    logger.info("I'm in function")
-    return 0
--- log/testmain.py
+++
@@ -1,9 +0,0 @@
-import log
-import function
-
-
-
-if __name__ == "__main__":
-    logger = log.set_log(module=__name__)
-    function.test2()
-    logger.info("im in main.py")
